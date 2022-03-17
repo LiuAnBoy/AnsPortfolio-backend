214cbd91ba3e4aed6ad5419f59508e67367719af
LiuAnBoy <eric141886@gmail.com>
2022-03-17 14:07:48 +0800

    be

--- client/src/presentations/components/HeaderBar.tsx
+++ client/src/presentations/components/HeaderBar.tsx
@@ -1,11 +1,18 @@
-import { Layout } from 'antd';
+import { Layout, Button, Avatar, Space, Typography } from 'antd';
 
 const { Header } = Layout;
 
 function HeaderBar() {
   return (
-    <Header>
+    <Header className="flex flex-row items-center justify-between">
       <div className="Logo text-white">An's Portfolio</div>
+
+      <span className='text-white'>env: {process.env.NODE_ENV}</span>
+
+      <Space align="baseline" size={16}>
+        <Avatar size="default">U</Avatar>
+        <span className="text-white">User</span>
+      </Space>
     </Header>
   );
 }
